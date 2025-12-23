982a69b88517850111db6a21fdebc4ee0d3418e4
Sagyntay Konratov <skonratov4@gmail.com>
2025-12-23 15:27:23 +0500

    save results in json

--- Analysis/views.py
+++ Analysis/views.py
@@ -1,7 +1,11 @@
 import requests
 import pandas as pd
+import matplotlib as plt
+from io import BytesIO
 from django.shortcuts import render
 from django.http import HttpResponse
+
+df = pd.read_json("data.json")
 
 
 API = "543f4eee-9541-4cc5-a5b7-19500e4e851a"
@@ -14,47 +18,39 @@ response = requests.request("GET", url, headers=headers, data=payload)
 
 # извлекаем данные
 data = response.json()
-pollution_data = data.get('data', {}).get('current', {}).get('pollution', {})
+pollution = data.get('data', {}).get('current', {}).get('pollution', {})
 weather = data.get('data', {}).get('current', {}).get('weather', {})
 
-city_info = {
-        #Загрязнение
-        'AQI (US)': pollution_data.get('aqius', 'N/A'), # Индекс качества воздуха (US)
-        'AQI (CN)': pollution_data.get('aqicn', 'N/A'), # Индекс качества воздуха (CN)
-        'Timestamp': pollution_data.get('ts', 'N/A'),
-        'Основной загрязнитель': pollution_data.get('maincn', 'N/A'),
-
-        #Погода
-        'Температура': weather.get('tp', 'N/A'),
-        'Влажность': weather.get('hu', 'N/A'),
-        'Давление': weather.get('pr', 'N/A'),
-        'Скорость ветра': weather.get('ws', 'N/A'),
-        'Направление ветра': weather.get('wd', 'N/A'),
-}
-
-df = pd.DataFrame([city_info])
-
-df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-#print(df)
-
-def air_quality(request):
-    data = {
+data = {
         "city": "Almaty",
         "state": "Almaty Oblysy",
         "country": "Kazakhstan",
+        "timestamp": pollution.get('ts', 'N/A'),
         "pollution": {
-            "aqius": df.iloc[0, 0],
-            "aqicn": df.iloc[0, 1],
-            #"mainus": df[""],
-            "maincn": df.iloc[0, 3],
+            "aqius": pollution.get('aqius', 'N/A'),
+            "aqicn": pollution.get('aqicn', 'N/A'),
+            "mainus": pollution.get('mainus', 'N/A'),
+            "maincn": pollution.get('maincn', 'N/A'),
         },
         "weather": {
-            "temperature": df.iloc[0, 4],
-            "humidity": df.iloc[0, 5],
-            "pressure": df.iloc[0, 6],
-            "wind_speed": df.iloc[0, 7],
-            "wind_direction": df.iloc[0, 8],
+            "temperature": weather.get('tp', 'N/A'),
+            "humidity": weather.get('hu', 'N/A'),
+            "pressure": weather.get('pr', 'N/A'),
+            "wind_speed": weather.get('ws', 'N/A'),
+            "wind_direction": weather.get('wd', 'N/A'),
             "icon": "50d",
         }
     }
+
+new_df = pd.DataFrame([data])
+new_df['timestamp'] = pd.to_datetime(df['timestamp'])
+df_updated = pd.concat([df, new_df], ignore_index=True)
+#print(df)
+df_updated.to_json("data.json")
+
+
+def air_quality(request):
     return render(request, "analysis/air.html", {"data": data})
+
+
+
