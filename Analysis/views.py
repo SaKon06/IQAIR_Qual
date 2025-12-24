e927d18318e5537d9f7639d9ebac29021590ed75
Sagyntay Konratov <skonratov4@gmail.com>
2025-12-24 21:56:48 +0500

    fixed api(us) plot

--- Analysis/views.py
+++ Analysis/views.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 from io import BytesIO
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.conf import settings
 
 df = pd.read_json("data.json")
-
+df['timestamp'] = pd.to_datetime(df['timestamp'], errors='coerce', utc=True)
+df['timestamp'] = df['timestamp'].dt.tz_convert('UTC').dt.tz_localize(None)
 
 API = "543f4eee-9541-4cc5-a5b7-19500e4e851a"
 url = f"http://api.airvisual.com/v2/city?city=Almaty&state=Almaty Oblysy&country=Kazakhstan&key={API}"
@@ -45,29 +47,35 @@ data = {
     }
 
 new_df = pd.DataFrame([data])
-new_df['timestamp'] = pd.to_datetime(df['timestamp'])
+new_df['timestamp'] = pd.to_datetime(new_df['timestamp'], errors='coerce', utc=True)
+new_df['timestamp'] = new_df['timestamp'].dt.tz_convert('UTC').dt.tz_localize(None)
 df_updated = pd.concat([df, new_df], ignore_index=True)
 #print(df)
-df_updated.to_json("data.json")
+df_updated.to_json("data.json", date_format='iso', orient='records')
 
 
 def air_quality(request):
     return render(request, "analysis/air.html", {"data": data})
 
 
+
+
+
+
 def air_plot(request):
 # убираем повторы и отсортировать
-    df_local = df_updated
-    df_local = df_local.drop_duplicates(subset='timestamp', keep='last')
-    df_local = df_local.sort_values('timestamp')
+    df_local = df_updated.drop_duplicates(subset='timestamp', keep='last').sort_values('timestamp')
+    df_local = df_local.set_index('timestamp')
 
+    df_local['aqius'] = df_local['pollution'].apply(lambda p: p.get('aqius') if isinstance(p, dict) else None).astype(float)
 
 # возьмём AQI (US) и timestamp из df
-    times = pd.to_datetime(df_local['timestamp'], unit='ms', errors='coerce')
-    aqi = df_local['pollution'].apply(lambda p: p.get('aqius') if isinstance(p, dict) else None)
+    #times = pd.to_datetime(df_local['timestamp'], unit='ms', errors='coerce')
+    daily = df_local['aqius'].resample('D').mean().dropna()
+    times = daily.index
+    aqi = daily.values
 
-
-    plt.figure(figsize=(10,3), dpi=120)
+    plt.figure(figsize=(8,3), dpi=120)
     plt.plot(times, aqi, marker='o', linestyle='-', color='#2c7fb8', linewidth=2)
     plt.fill_between(times, aqi, alpha=0.1, color='#2c7fb8')
     plt.title('AQI (US) — история')
